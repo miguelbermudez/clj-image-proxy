96057ccb18e02eed1db476a7b7ef4c0df0f0ca67
Miguel Bermudez <gmiggs@gmail.com>
2013-07-31 23:06:42 -0400

    everything working, still needs more tests

--- src/image_proxy/core.clj
+++ src/image_proxy/core.clj
@@ -12,34 +12,31 @@
 (import [javax.imageio ImageIO])
 
 (defn get-file "Get file from url as byte array" [url]
-  (client/get url {:as :byte-array})))
+  (client/get url {:as :byte-array}))
 
 (defn get-base64-img-str "Convert an image to base64 string" [bArray]
   (String. (Base64/encodeBase64 bArray)))
 
 (defn proccess-image "create java image for basic properties" [bArray]
-  (ImageIO/read (java.io.ByteArrayInputStream. (bArray))))
+  (ImageIO/read (java.io.ByteArrayInputStream. bArray)))
 
 (defn get-img "return base64 img str, width, height as json" [url]
   (response
     (let
-      [imgBArray (get-file url)]
-      [b64Str (get-base64-img-str imgBArray)]
-      [img (proccess-image imgBArray)]
+      [imgReq (get-file url)
+       imgBArray (:body imgReq)
+       b64Str (get-base64-img-str imgBArray)
+       img (proccess-image imgBArray)
+       imageType (get (:headers imgReq) "content-type")]
       {:width (.getWidth img)
        :height (.getHeight img)
-       :data  ()})
-    ))
-
-
-
-
+       :data  (str "data:" imageType ";base64," b64Str)})))
 
 (defroutes app-routes
   ; to serve document root address
   (GET "/" [] "<h1>Hello from Image-Proxy")
-  (GET "/image" {params :params} (pr-str params))
-
+  (GET "/imagetest" {params :params} (pr-str params))
+  (GET "/image" {params :params} (get-img (:url params)))
 
   ; to server static pages saved in resources/public directory
   (route/resources "/")
@@ -49,4 +46,4 @@
 (def app
   (-> (handler/api app-routes)
       (middleware/wrap-json-body)
-      (middlew(are/wrap-json-response)))
+      (middleware/wrap-json-response)))
--- test/image_proxy/core_test.clj
+++ test/image_proxy/core_test.clj
@@ -1,3 +1,22 @@
 (ns image-proxy.core-test
   (:require [clojure.test :refer :all]
+            [clj-http.client :as client]
             [image-proxy.core :refer :all]))
+
+(defn setup []
+  (println "setup ...")
+  (def imgReq (client/get "https://www.google.com/images/srpr/logo4w.png" {:as :byte-array})))
+
+(defn teardown []
+  (println "teardown ..."))
+
+(defn url-file-fixture [f]
+  (setup)
+  (println (type f))
+  (f)
+  (teardown))
+
+(use-fixtures :once url-file-fixture)
+
+(deftest get-file-test
+  (is (true? imgReq)))
